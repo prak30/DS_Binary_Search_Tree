dfacea6970035ad6802a8520ff24c7f66f5c5002
pranav <prnvkatkar88@gmail.com>
2021-10-22 23:53:25 +0530

    Added:craeted multilevel bst

--- MyBinaryTreeImplementation.java
+++ MyBinaryTreeImplementation.java
@@ -6,6 +6,16 @@ public class MyBinaryTreeImplementation {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
 		int size = myBinaryTree.getSize();
 		System.out.println("Size is :" + size);
 	}
